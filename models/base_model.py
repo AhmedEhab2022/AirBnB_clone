3ca50dc57502a6cd4dfd78bf1b85a69ed8da336c
Mohammed Shousha <mshousha29@gmail.com>
2023-10-10 15:34:47 +0300

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -19,11 +19,21 @@ class BaseModel():
         to_dict(self)
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the instance"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string representation of the instance"""
