1ca67de350ab414a1dff28df368f1c6a8c0753d2
Mohammed Shousha <mshousha29@gmail.com>
2023-10-11 20:42:00 +0300

    models tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,71 +3,61 @@
 
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel class"""
 
     def setUp(self):
-        """Sets up test methods."""
-        pass
+        """Sets up BaseModel for testing"""
+        self.bm = BaseModel()
 
     def tearDown(self):
-        """Tears down test methods."""
-        pass
+        """Tears down BaseModel testing"""
+        del self.bm
 
-    def test_instantiation(self):
-        """Tests instantiation of BaseModel class"""
-        bm = BaseModel()
-        self.assertIsInstance(bm, BaseModel)
-        self.assertTrue(issubclass(type(bm), BaseModel))
+    def test_instance(self):
+        """Tests BaseModel instance"""
+        self.assertIsInstance(self.bm, BaseModel)
 
     def test_id(self):
         """Tests id attribute"""
-        bm = BaseModel()
-        self.assertTrue(hasattr(bm, "id"))
-        self.assertIsInstance(bm.id, str)
+        self.assertTrue(hasattr(self.bm, "id"))
+        self.assertIsInstance(self.bm.id, str)
 
     def test_created_at(self):
         """Tests created_at attribute"""
-        bm = BaseModel()
-        self.assertTrue(hasattr(bm, "created_at"))
-        self.assertIsInstance(bm.created_at, datetime)
+        self.assertTrue(hasattr(self.bm, "created_at"))
+        self.assertEqual(type(self.bm.created_at).__name__, "datetime")
 
     def test_updated_at(self):
         """Tests updated_at attribute"""
-        bm = BaseModel()
-        self.assertTrue(hasattr(bm, "updated_at"))
-        self.assertIsInstance(bm.updated_at, datetime)
+        self.assertTrue(hasattr(self.bm, "updated_at"))
+        self.assertEqual(type(self.bm.updated_at).__name__, "datetime")
 
     def test_save(self):
         """Tests save method"""
-        bm = BaseModel()
-        bm.save()
-        self.assertNotEqual(bm.created_at, bm.updated_at)
+        self.bm.save()
+        self.assertNotEqual(self.bm.created_at, self.bm.updated_at)
 
     def test_str(self):
         """Tests __str__ method"""
-        bm = BaseModel()
-        bm_str = bm.__str__()
+        bm_str = self.bm.__str__()
         self.assertEqual(bm_str,
-                         "[BaseModel] ({}) <{}>".format(bm.id, bm.__dict__))
+                         "[BaseModel] ({}) {}".format(self.bm.id, self.bm.__dict__))
 
     def test_to_dict(self):
         """Tests to_dict method"""
-        bm = BaseModel()
-        bm_dict = bm.to_dict()
+        bm_dict = self.bm.to_dict()
         self.assertEqual(bm_dict["__class__"], "BaseModel")
         self.assertEqual(type(bm_dict["created_at"]), str)
         self.assertEqual(type(bm_dict["updated_at"]), str)
 
     def test_init_from_dict(self):
         """Tests init from dictionary"""
-        bm = BaseModel()
-        bm_dict = bm.to_dict()
+        bm_dict = self.bm.to_dict()
         bm2 = BaseModel(**bm_dict)
-        self.assertEqual(bm.id, bm2.id)
-        self.assertEqual(bm.created_at, bm2.created_at)
-        self.assertEqual(bm.updated_at, bm2.updated_at)
-        self.assertEqual(bm.__dict__, bm2.__dict__)
+        self.assertEqual(self.bm.id, bm2.id)
+        self.assertEqual(self.bm.created_at, bm2.created_at)
+        self.assertEqual(self.bm.updated_at, bm2.updated_at)
+        self.assertEqual(self.bm.__dict__, bm2.__dict__)
